e98d6ed6e3fb55201e58d2280d392b6fd140ff40
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-06-22 14:06:38 -0700

    Add project files.

---
+++ AlmostPre5/Abstaction/HellowClient.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmostPre5.Abstaction
+{
+    class HellowClient
+    {
+        HttpClient client = new HttpClient();
+        public HellowClient()
+        {
+            
+        }
+
+        public async Task<HttpResponseMessage> PostNewArticle(Post post)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(post), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("192.168.86.151:100000/article", content);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteArticle(string id)
+        {
+            StringContent content = new StringContent($""
+        }
+    }
+}
---
+++ AlmostPre5/Abstaction/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmostPre5.Abstaction
+{
+    class Post
+    {
+        string Id;
+        string Title;
+        string Desc;
+        string Content;
+    }
+}
